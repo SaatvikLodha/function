eea669ebf1589c660afc49fc485cc3e1fd4654c0
SaatvikLodha <saatviklodha@gmail.com>
2026-07-10 15:26:55 -0500

    Add files via upload

---
+++ wellwishes.py
@@ -0,0 +1,9 @@
+def introduction():
+    print("Hello, my name is Saatvik!")
+    print("I am 12 years old,")
+    print("and I am going to 7th grade.")
+introduction()
+
+def wellwishes():
+    print("Wishing you a wonderful day filled with joy and happiness!")
+wellwishes()
